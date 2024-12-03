9e2d6de065ab4bb664de667b09f0831bfc2af808
Dearbhla <dearbhlalane@gmail.com>
2024-12-03 17:53:58 +0000

    background

--- musicassignment/button1.gd
+++ musicassignment/button1.gd
@@ -1,6 +1,8 @@
 extends Button
 
 @export var sound: Node 
+ 
+var counter =0 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
--- musicassignment/music.gd
+++ musicassignment/music.gd
@@ -1,5 +1,8 @@
 extends Node2D
 
+@export
+
+var counter = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
